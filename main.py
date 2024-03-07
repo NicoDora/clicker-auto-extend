f4e1ebfbd0a1fa12d08e4b31cdb4e2cf8eceadac
NicoDora <pjh76574734@gmail.com>
2024-03-07 21:16:17 +0900

    🚨 bugfix: 자리 선택 기능 버그 수정

--- main.py
+++ main.py
@@ -9,15 +9,36 @@ import re
 user_id = "" # 아이디 입력
 user_pass = "" # 비밀번호 입력
 
-action_code = "1" # 자리 선택: 0, 자리 이동: 1
-
-seat_id = "" # 현재 내 좌석 번호 or 원하는 좌석 번호 입력
+action_code = "0" # 자리 선택: 0, 자리 이동: 1
+seat_number = "" # 현재 내 좌석 번호 or 원하는 좌석 번호 입력
 
 seat_reservation_url = "https://seat.induk.ac.kr/Clicker/ReadingRoomAction" # 좌석 예약 페이지 주소
 
 lib_first_floor_id = "20240130112852987" # 도서관 1층 아이디
-
 lib_url = f"https://seat.induk.ac.kr/Clicker/UserSeat/{ lib_first_floor_id }?DeviceName=normal"
+
+def transformation_seat_number_to_seat_id(seat_number):
+  try:
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--disable-gpu")
+    chrome_options.add_argument("user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36")
+    chrome_options.add_argument("lang=ko_KR")
+
+    driver = webdriver.Chrome(options=chrome_options)
+
+    driver.get(lib_url)
+
+    wait = WebDriverWait(driver, 10)
+
+    seat_number_locate = wait.until(EC.presence_of_element_located((By.XPATH, f'//span[@class="clicker_s_s_no" and text()="{ seat_number }"]')))
+    parent_div = seat_number_locate.find_element(By.XPATH, "./parent::div")
+    seat_full_id = parent_div.get_attribute('id')
+    seat_id = re.search(r'\d+$', seat_full_id).group()
+    return seat_id
+
+  except Exception as error:
+    return { "error": error }
 
 def find_available_seat():
   try:
@@ -35,10 +56,10 @@ def find_available_seat():
 
     element = wait.until(EC.presence_of_element_located((By.XPATH, '//div[@id="clicker_div_guide_map"]//div[@title="배정가능"]'))).get_attribute('id')
 
-    my_seat_number = wait.until(EC.element_to_be_clickable((By.XPATH, f'//span[@class="clicker_s_s_no" and text()="{ seat_id }"]')))
+    my_seat_number_locate = wait.until(EC.element_to_be_clickable((By.XPATH, f'//span[@class="clicker_s_s_no" and text()="{ seat_number }"]')))
 
     # 상위 div 찾기
-    parent_div = my_seat_number.find_element(By.XPATH, "./parent::div")
+    parent_div = my_seat_number_locate.find_element(By.XPATH, "./parent::div")
 
     # 상위 div의 id 값 가져오기
     my_seat_full_id = parent_div.get_attribute('id')
@@ -73,12 +94,16 @@ def request_seat(seat_id):
     return { "error": "request error" }
 
 if __name__ == "__main__":
+
   if action_code == '0':
+    seat_id = transformation_seat_number_to_seat_id(seat_number)
     result = request_seat(seat_id)
+
     if result:
       print("예약 성공")
     else:
       print("예약 실패")
+
   elif action_code == '1':
     # 선택가능한 좌석 아이디 찾기
     my_seat_id, next_seat_id = find_available_seat()
