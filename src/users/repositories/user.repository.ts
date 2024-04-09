8d8e1db83ef506bc7b40ea4b9a0ca3bd61e8098f
NicoDora <pjh76574734@gmail.com>
2024-04-09 11:51:14 +0900

    ✏️ modify(#3): 로그인 에러 핸들링

--- src/users/repositories/user.repository.ts
+++ src/users/repositories/user.repository.ts
@@ -1,12 +1,24 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { User } from 'src/entities/User';
 import { EntityManager } from 'typeorm';
+import { QueryFailedError } from 'typeorm';
 
 @Injectable()
 export class UserRepository {
   constructor(private readonly entityManager: EntityManager) {}
 
-  createUser(stdNum: string, password: string) {
-    return this.entityManager.save(User, { stdNum, password });
+  async createUser(stdNum: string, password: string) {
+    try {
+      return await this.entityManager.save(User, { stdNum, password });
+    } catch (error) {
+      if (
+        error instanceof QueryFailedError &&
+        error.message.includes('ER_DUP_ENTRY')
+      ) {
+        throw new HttpException('User already exists', HttpStatus.CONFLICT);
+      } else {
+        console.error(error);
+      }
+    }
   }
 }
